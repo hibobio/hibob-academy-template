43bb73f17d98911f0ff463be64a618fd3b1bdafa
Ron Azar <ronazar1999@gmail.com>
2024-09-03 11:26:19 +0300

    Fixed my Q2 other solution

--- src/main/kotlin/com/hibob/KotlinBasics/extentions.kt
+++ src/main/kotlin/com/hibob/KotlinBasics/extentions.kt
@@ -1,3 +1,4 @@
+package com.hibob.KotlinBasics
 //Q1
 fun List<Int>.sum():Int{
     var result = 0
@@ -15,17 +16,17 @@ fun main(args:Array<String>){
 
 //Q2
 infix fun Number.toPowerOf(exponent: Number):Double{
-    //Math.pow(this.toDouble(), exponent.toDouble())
-  My solution:
-    val pow = 2
-    var retValue = this
-    if (exponent == 0)
-        return 1
-    for (pow in 2..exponent) {
-        retValue *= this
-    }
-    if (exponent>=1)
-        return retValue
-    else
-        return 1/retValue
+    Math.pow(this.toDouble(), exponent.toDouble())
+//  //Other solution:
+//    val pow = 2
+//    var retValue = this
+//    if (exponent == 0)
+//        return 1
+//    repeat((exponent.toInt() - 1)) {
+//        retValue *= this
+//    }
+//    if (exponent>=1)
+//        return retValue
+//    else
+//        return 1/retValue
 }
