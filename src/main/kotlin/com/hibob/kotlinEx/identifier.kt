9509a2bb1ec15d9fae9af0751add7ded2a46cadd
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-03 16:06:10 +0300

    reuploading identifier branch and file

---
+++ src/main/kotlin/com/hibob/kotlinEx/identifier.kt
@@ -0,0 +1,31 @@
+package com.hibob.kotlinEx
+
+fun isValidIdentifier(s: String): Boolean {
+    if (s === "" || isDigit(s[0]) || !(isLetter(s[0]) || s[0] == '_')) {
+        return false
+    }
+    for (i in 1 until s.length) {
+        val c = s[i]
+        if (!(isDigit(c) || isLetter(c)|| s[0] == '_')) {
+            return false
+        }
+    }
+    return true
+}
+
+private fun isLetter(c: Char): Boolean {
+    return c in 'a'..'z' || c in 'A'..'Z'
+}
+
+private fun isDigit(c: Char): Boolean {
+    return c in '0'..'9'
+}
+
+fun main(args: Array<String>) {
+    println(isValidIdentifier("name"))   // true
+    println(isValidIdentifier("_name"))  // true
+    println(isValidIdentifier("_12"))    // true
+    println(isValidIdentifier(""))       // false
+    println(isValidIdentifier("012"))    // false
+    println(isValidIdentifier("no$"))    // false
+}
