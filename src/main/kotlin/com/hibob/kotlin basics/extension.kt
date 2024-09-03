d250bc7bb580672024166519428c9284b2e924dd
adi.finkelman <adi.finkelman@hibob.io>
2024-09-03 11:07:03 +0300

    Create sum and toPowerOf

--- src/main/kotlin/com/hibob/kotlin basics/extension.kt
+++ src/main/kotlin/com/hibob/kotlin basics/extension.kt
@@ -1,3 +1,25 @@
-fun test() {
-    val a = 2
+fun sum(list: List<Int>): Int {
+    var result = 0
+    for (i in list) {
+        result += i
+    }
+    return result
+}
+
+infix fun Number.toPowerOf(num: Number, exponent: Number): Double {
+    var result = 1;
+    for (i in 1..exponent)
+        result *= num
+    return result
+}
+
+fun main(args: Array<String>) {
+    val sum = sum(listOf(1,2,3))
+    println(sum)
+    fun List.sum(list: List<Int>): Int {
+        var result = 0
+        for (i in list)
+            result += i
+    }
+    return result
 }
