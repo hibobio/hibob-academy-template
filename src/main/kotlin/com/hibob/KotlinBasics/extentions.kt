892f46a38e20c826287c6716b635174f454cfdd9
Ron Azar <ronazar1999@gmail.com>
2024-09-03 11:08:16 +0300

    Answering Q1+Q2 Creating 2 extensions

--- src/main/kotlin/com/hibob/KotlinBasics/extentions.kt
+++ src/main/kotlin/com/hibob/KotlinBasics/extentions.kt
@@ -1,3 +1,31 @@
-fun test(){
+//Q1
+fun List.sum():Int{
+    var result = 0
+    for (i in this){
+        result+=i
+    }
+    return result
+}
 
+fun main(args:Array<String>){
+    val sum = listOf(1,2,3).sum()
+    println(sum) //6
+}
+
+
+//Q2
+infix fun Number.toPowerOf(exponent: Number):Double{
+    this.pow(exponent)
+//  My solution:
+//    val pow = 2
+//    var retValue = this
+//    if (exponent == 0)
+//        return 1
+//    for (pow in 2..exponent) {
+//        retValue *= this
+//    }
+//    if (exponent>=1)
+//        return retValue
+//    else
+//        return 1/retValue
 }
