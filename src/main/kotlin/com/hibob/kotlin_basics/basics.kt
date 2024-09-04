0760b16988af33a03eb7ba53336c50b3bdb76ccb
adi.finkelman <adi.finkelman@hibob.io>
2024-09-04 20:54:34 +0300

    After delete commit

--- src/main/kotlin/com/hibob/kotlin_basics/basics.kt
+++ src/main/kotlin/com/hibob/kotlin_basics/basics.kt
@@ -1,52 +1,40 @@
 package com.hibob.kotlin_basics
-//Write a “hello world” program
-//Write a function that prints if the given number is even or odd
-//Write a function that checks whether two integers are equal or not
-//Write a function as an expression (one line) that prints the max value of two given integers
-//Write as many possibilities as you can to call
-//fun multiplication(a: Int = 1, b: Int = 1): Int = a * b
-//with parameters
-//a = 1, b= 5
-//a = 1, b= 1
-//a=3, b= 2
 
 fun main() {
-    // 1
-    println("Hello World")
-
-    // 2
-    println("Even or odd: " + evenOrOdd(1))
-    println("Even or odd: " + evenOrOdd(2))
-
-    // 3
-    println("Are equals: " + areNumbersEquals(1,2))
-    println("Are equals: " + areNumbersEquals(2,2))
-
-    // 4
-    print("Max: ")
-    printMaxValue(11,10)
-
-    // 5
-    println("Multiplication: " + multiplication(1,5))
-    // a = 5 b = 1
-    println("Multiplication: " + multiplication(5))
-    println("Multiplication: " + multiplication(1,1))
-    // a = 1 b = 1
-    println("Multiplication: " + multiplication())
-    println("Multiplication: " + multiplication(3,2))
+    //Q1
+    printHelloWorld()
+    //Q2
+    println(evenOrOdd(2))
+    println(evenOrOdd(3))
+    //Q3
+    println(equalNumbers(2,2))
+    println(equalNumbers(2,3))
+    //Q4
+    println(maxValue(11,10))
+    //a=1, b =5 ===> 5
+    println(multiplication(1,5))
+    println(multiplication(5))
+    //a=1, b = 1 ===> 1
+    println(multiplication(1,1))
+    println( multiplication())
+    //a=3, b=2 ===> 6
+    println(multiplication(3,2))
+    println(multiplication(6))
 }
-
+fun printHelloWorld() {
+    println("Hello World!")
+}
 fun evenOrOdd(num : Int) : String {
     if (num % 2 == 0) {
         return "Even"
     }
     return "Odd"
 }
-fun areNumbersEquals(n1: Int, n2: Int) : String {
-    if (n1 == n2)
+fun equalNumbers(num1: Int, num2: Int) : String {
+    if (num1 == num2) {
         return "Equal"
+    }
     return "Not Equal"
 }
-fun printMaxValue(num1: Int, num2: Int) = if (num1 > num2) println(num1) else println(num2)
-
+fun maxValue(num1: Int, num2: Int) : Int = if (num1 > num2) num1 else num2
 fun multiplication(a: Int=1, b: Int=1): Int = a * b
