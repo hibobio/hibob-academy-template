fde9a964ba030bf59e68c956a63283cef13c6ff6
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-05 10:58:46 +0300

    fixing problems

--- src/main/kotlin/com/hibob/kotlinEx/extension.kt
+++ src/main/kotlin/com/hibob/kotlinEx/extension.kt
@@ -1,13 +1,5 @@
 package com.hibob.kotlinEx
-
-//change the 'sum\ function so that it was declared as an extension to List<Int>
-fun sum(list: List<Int>): Int{
-    var result = 0
-    for (i in list){
-        result +=i
-    }
-    return result
-}
+import kotlin.math.pow
 
 fun List<Int>.sum(): Int {
     var result = 0
@@ -17,18 +9,15 @@ fun List<Int>.sum(): Int {
     return result
 }
 
-//write the following program method as an infix extension function
-infix fun Number.toPowerOf(num: Number, exponent: Number): Double {
-    var result: Double = 1
-    for (i in 0 until exponent) {
-        result = result*num
-    }
-    return result
+infix fun Number.toPowerOf(exponent: Number): Double {
+    return this.toDouble().pow(exponent.toDouble())
 }
 
 
 fun main(args: Array<String>) {
 
     val sum = listOf(1, 2, 3).sum()
+    val x: Number = 2
     println(sum)
+    println(x.toPowerOf(2))
 }
