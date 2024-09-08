acb5c501750597a1d88ea43ef808acfd3b7006a0
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-08 15:49:19 +0300

    SQL part 1 exercise - commenting some of the commands

--- src/main/resources/db/migration/V202408091400__create_pets_table.sql
+++ src/main/resources/db/migration/V202408091400__create_pets_table.sql
@@ -11,8 +11,8 @@ INSERT INTO pets VALUES (1,"Murphy","dog",1,"2024-09-08");
 INSERT INTO pets VALUES(2,"Pepe","dog",1,"2024-09-09");
 INSERT INTO pets VALUES(1,"Garfield","cat",2,"2024-09-08");
 
-SELECT * from pets where type = 'dog';
+-- SELECT * from pets where type = 'dog';
 
 delete from pets where name='Garfield'
 
-SELECT * from pets where date_of_arrival<'2023-09-08';
+-- SELECT * from pets where date_of_arrival<'2023-09-08';
