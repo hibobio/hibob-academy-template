c46bba953bbf034aff72d16541863c1a4fd132aa
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-08 15:43:50 +0300

    SQL part 1 exercise - done

--- src/main/resources/db/migration/V202408091400__create_pets_table.sql
+++ src/main/resources/db/migration/V202408091400__create_pets_table.sql
@@ -1,10 +1,18 @@
 create table pets
 (
     id SERIAL primary key ,
-    name text,
-    type varchar,
-    company_id int,
+    name varchar(255),
+    type varchar(255),
+    company_id integer,
     date_of_arrival date
-)
+);
 
--- INSERT INTO pets
+INSERT INTO pets VALUES (1,"Murphy","dog",1,"2024-09-08");
+INSERT INTO pets VALUES(2,"Pepe","dog",1,"2024-09-09");
+INSERT INTO pets VALUES(1,"Garfield","cat",2,"2024-09-08");
+
+SELECT * from pets where type = 'dog';
+
+delete from pets where name='Garfield'
+
+SELECT * from pets where date_of_arrival<'2023-09-08';
