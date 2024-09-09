102117fa8a5b4126165b82d7f0a2bfd7f842268a
adi.finkelman <adi.finkelman@hibob.io>
2024-09-09 15:05:41 +0300

    String vaildator

---
+++ src/main/kotlin/com/hibob/kotlin_basics/StringVaildator.kt
@@ -0,0 +1,18 @@
+package com.hibob.kotlin_basics
+
+fun isValidIdentifier(s: String): Boolean {
+    if (s.isNotEmpty() && (s[0] == '_' || s[0].isLetter()))
+        return s.drop(1).all {
+            it.isLetterOrDigit()
+        }
+    return false
+}
+
+fun main(args: Array<String>) {
+    println(isValidIdentifier("name"))   // true
+    println(isValidIdentifier("_name"))  // true
+    println(isValidIdentifier("_12"))    // true
+    println(isValidIdentifier(""))       // false
+    println(isValidIdentifier("012"))    // false
+    println(isValidIdentifier("no$"))    // false
+}
