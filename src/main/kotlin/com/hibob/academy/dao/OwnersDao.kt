2545a0905390193cf2885a0a9a4fc800db186e1e
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-10 21:12:05 +0300

    rebasing

--- src/main/kotlin/com/hibob/academy/dao/OwnersDao.kt
+++ src/main/kotlin/com/hibob/academy/dao/OwnersDao.kt
@@ -5,38 +5,18 @@ import com.hibob.academy.utils.asIs
 import org.jooq.DSLContext
 import org.springframework.stereotype.Component
 import com.hibob.academy.utils.from
-import org.jooq.Record
-import org.jooq.RecordMapper
-import org.jooq.impl.DSL
 
 @Component
 class OwnersDao(private val sql: DSLContext) {
 
     private val table = OwnersTable.instance
 
-    private val ownerMapper = RecordMapper<Record, Owner> {
-        record ->
-        Owner(record[table.id],
-            record[table.name],
-            record[table.companyId],
-            record[table.employeeId]
-        )
-    }
-
     fun getAllOwners(): List<Owner> {
         return sql.select(table.name, table.employeeId, table.companyId)
             .from(table)
-            .fetch(ownerMapper)
-    }
-
-    fun insertOwner(owner: Owner) {
-        sql.insertInto(table)
-        .set(table.id, owner.id)
-        .set(table.name, owner.name)
-        .set(table.companyId, owner.companyId)
-        .set(table.employeeId, owner.employeeId)
-        .onConflict(table.companyId, table.employeeId)
-        .doNothing()
-        .execute()
+            .fetch()
+            .map { record ->
+                mapToOwner(record)
+            }
     }
 }
