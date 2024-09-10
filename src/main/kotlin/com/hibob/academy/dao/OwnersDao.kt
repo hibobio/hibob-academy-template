ec78add6b91a3ef1157ef704e771486b642d765d
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-10 21:12:05 +0300

    dao done

--- src/main/kotlin/com/hibob/academy/dao/OwnersDao.kt
+++ src/main/kotlin/com/hibob/academy/dao/OwnersDao.kt
@@ -5,36 +5,18 @@ import com.hibob.academy.utils.asIs
 import org.jooq.DSLContext
 import org.springframework.stereotype.Component
 import com.hibob.academy.utils.from
-import org.jooq.Record
-import org.jooq.RecordMapper
-import org.jooq.impl.DSL
 
 @Component
 class OwnersDao(private val sql: DSLContext) {
 
     private val table = OwnersTable.instance
 
-    private val ownerMapper = RecordMapper<Record, OwnerData> {
-        record ->
-        OwnerData(record[table.name],
-            record[table.companyId],
-            record[table.employeeId]
-        )
-    }
-
-    fun getAllOwners(): List<OwnerData> {
+    fun getAllOwners(): List<Owner> {
         return sql.select(table.name, table.employeeId, table.companyId)
             .from(table)
-            .fetch(ownerMapper)
-    }
-
-    fun insertOwner(owner: OwnerData) {
-        sql.insertInto(table)
-        .set(table.name, owner.name)
-        .set(table.companyId, owner.companyId)
-        .set(table.employeeId, owner.employeeId)
-        .onConflict(table.companyId, table.employeeId)
-        .doNothing()
-        .execute()
+            .fetch()
+            .map { record ->
+                mapToOwner(record)
+            }
     }
 }
--- src/main/kotlin/com/hibob/academy/dao/PetsDao.kt
+++ src/main/kotlin/com/hibob/academy/dao/PetsDao.kt
@@ -5,9 +5,6 @@ import com.hibob.academy.utils.asIs
 import org.jooq.DSLContext
 import org.springframework.stereotype.Component
 import com.hibob.academy.utils.from
-import org.jooq.Record
-import org.jooq.RecordMapper
-import java.time.LocalDate
 
 enum class PetType(val type: String) {
     DOG("dog"),
@@ -16,24 +13,16 @@ enum class PetType(val type: String) {
     FISH("fish");
 }
 
+
 @Component
 class PetsDao(private val sql: DSLContext) {
 
     private val table = PetsTable.instance
 
-    private val petMapper = RecordMapper<Record, PetData> {
-            record ->
-        PetData(record[table.name],
-            record[table.type],
-            record[table.companyId])
-    }
-
-    fun petsByType(type: PetType) : List<PetData> {
-        return sql.select(table.name, table.dateOfArrival, table.companyId)
+    fun petsByType(type: PetType) : List<Pet> {
+        return sql.select(table.name, table.date_of_arrival, table.companyId)
             .from(table)
             .where(table.type.eq(type.type))
-            .fetch(petMapper)
+            .fetch().map { record3 -> mapToPet(record3) }
     }
-
-//    fun
 }
