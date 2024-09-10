3448625591f4c1a41b346546bbdb48f86bf7cb41
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-10 11:27:52 +0300

    SQL1 - fixing issues

--- src/main/resources/db/migration/V202408091400__create_pets_table.sql
+++ src/main/resources/db/migration/V202408091400__create_pets_table.sql
@@ -4,15 +4,17 @@ create table pets
     name varchar(255),
     type varchar(255),
     company_id integer,
-    date_of_arrival date
+    date_of_arrival date default current_date
 );
 
-INSERT INTO pets VALUES (1,"Murphy","dog",1,"2024-09-08");
-INSERT INTO pets VALUES(2,"Pepe","dog",1,"2024-09-09");
-INSERT INTO pets VALUES(1,"Garfield","cat",2,"2024-09-08");
+CREATE INDEX idx_company_id ON pets (company_id);
+
+-- INSERT INTO pets VALUES (1,"Murphy","dog",1,"2024-09-08");
+-- INSERT INTO pets VALUES(2,"Pepe","dog",1,"2024-09-09");
+-- INSERT INTO pets VALUES(1,"Garfield","cat",2,"2024-09-08");
 
 -- SELECT * from pets where type = 'dog';
-
-delete from pets where name='Garfield'
-
--- SELECT * from pets where date_of_arrival<'2023-09-08';
+-- delete from pets where id=1
+-- SELECT *
+-- FROM pets
+-- WHERE date_of_arrival < CURDATE() - INTERVAL 1 YEAR;
--- src/main/resources/db/migration/V202408091500__create_owners_table.sql
+++ src/main/resources/db/migration/V202408091500__create_owners_table.sql
@@ -1,7 +1,10 @@
 create table owners
 (
     id serial primary key,
-    name text,
+    name varchar,
     company_id int,
-    employee_id int
+    employee_id varchar
 )
+
+-- CREATE INDEX idx_company_id ON owners (company_id);
+-- CREATE INDEX idx_employee_id ON pets (employee_id);
