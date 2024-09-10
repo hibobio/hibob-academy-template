7696005ba66bbc50e4d67ccb03bdc92e3f25f9ff
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-10 13:15:11 +0300

    rebasing from jooq read to sql part 1

--- src/main/kotlin/com/hibob/academy/dao/tables.kt
+++ src/main/kotlin/com/hibob/academy/dao/tables.kt
@@ -1,13 +1,20 @@
 package com.hibob.academy.dao
+
 import com.hibob.academy.utils.JooqTable
+import javassist.CtMethod.ConstParameter.integer
+import java.io.Serial
 
 class PetsTable(tableName : String = "pets") : JooqTable(tableName) {
-    val id = createBigIntField("id")
+    val id = createIntField("id")
     val name = createVarcharField("name")
     val type = createVarcharField("type")
-    val companyId = createVarcharField("company_Id")
-    val dateOfArrival = createDateField("date_of_arrival")
 
+
+//    id SERIAL primary key ,
+//    name varchar(255),
+//    type varchar(255),
+//    company_id integer,
+//    date_of_arrival date default current_date
 
     companion object{
         val instance = PetsTable()
@@ -15,12 +22,6 @@ class PetsTable(tableName : String = "pets") : JooqTable(tableName) {
 }
 
 class OwnersTable(tableName : String = "owners") : JooqTable(tableName) {
-
-    val id = createIntField("id")
-    val name = createVarcharField("name")
-    val companyId = createBigIntField("company_id")
-    val employeeId = createVarcharField("employee_id")
-
     companion object{
         val instance = OwnersTable()
     }
