58d11eac26908ec97784fdd81050f4847f2ca663
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-11 13:45:58 +0300

    tests for dao

---
+++ src/test/kotlin/com/hibob/academy/dao/OwnersDaoTest.kt
@@ -0,0 +1,16 @@
+package com.hibob.academy.dao
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class OwnersDaoTest {
+
+    @Test
+    fun getAllOwners() {
+    }
+
+    @Test
+    fun insertOwner() {
+    }
+}
