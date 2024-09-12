41f58ae34d4a9ceccdb5e15f058f85652d150a5a
Boaz <boaz.cohen@hibob.io>
2024-09-12 15:00:51 +0300

    remove extra line

--- src/main/resources/db/migration/V202409081500__create_owner_table.sql
+++ src/main/resources/db/migration/V202409081500__create_owner_table.sql
@@ -6,5 +6,4 @@ create table owner
     name varchar(255) NOT NULL
 );
 
-CREATE INDEX idx_owner_company_id_employee_id on owner(company_id, employee_id)
-
+CREATE INDEX idx_owner_company_id_employee_id on owner(company_id, employee_id);
