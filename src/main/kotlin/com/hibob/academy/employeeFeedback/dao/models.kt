ef05935ba306c71227e3b9704b2e16e349620712
chezi-nikop <yechezkelnikop@gmail.com>
2024-09-24 13:16:29 +0300

    models - data class (#37)
    * finish with migration
    
    * finish with migration
    
    * finish with data class
    
    * fixed
    
    * added employee and also convert enumToString and stringToEnum
    
    * added hr

---
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/dao/models.kt
@@ -0,0 +1,62 @@
+package com.hibob.academy.employeeFeedback.dao
+
+import java.time.LocalDate
+
+data class FeedbackIn(
+    val employeeId: Long?,
+    val content: String,
+    val status: FeedbackStatus = FeedbackStatus.UNREVIEWED,
+    val companyId: Long
+)
+
+data class FeedbackOut(
+    val id: Long,
+    val employeeId: Long?,
+    val content: String,
+    val status: FeedbackStatus,
+    val companyId: Long,
+    val date: LocalDate
+)
+
+data class EmployeeIn(
+    val firstName: String,
+    val lastName: String,
+    val role: RoleType,
+    val companyId: Long,
+)
+
+data class EmployeeOut(
+    val id: Long,
+    val firstName: String,
+    val lastName: String,
+    val role: RoleType,
+    val companyId: Long,
+)
+
+enum class RoleType {
+    ADMIN,
+    MANAGER,
+    EMPLOYEE,
+    HR;
+
+    companion object {
+        fun enumToString(role: RoleType): String =
+            role.toString()
+
+        fun stringToEnum(string: String): RoleType =
+            valueOf(string.uppercase())
+    }
+}
+
+enum class FeedbackStatus {
+    REVIEWED,
+    UNREVIEWED;
+
+    companion object {
+        fun enumToString(status: FeedbackStatus): String =
+            status.toString()
+
+        fun stringToEnum(string: String): FeedbackStatus =
+            valueOf(string.uppercase())
+    }
+}
